8603e54caee00cd5176b06b36c601c3544c3985d
James Hooper <jameshooperdesigns@gmail.com>
2019-01-28 14:35:08 -0700

    Update application

---
+++ app.js
@@ -0,0 +1,28 @@
+// CRUD Server - Create, Read, Update, and Delete
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const dev_db_url = 'mongodb+srv://jhooperdev:servingtheweb2019!$@product-cluster-iyok6.gcp.mongodb.net/product'
+const mongoDB = process.env.MONGODB_URI || dev_db_url
+mongoose.connect(mongoDB, {useNewUrlParser: true})
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const product = require('./routes/product.route')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/products', product)
+
+const port = 5775
+
+app.listen(port, () => {
+    console.log(`Server is up and running on ${port}`)
+})
+
---
+++ routes/product.route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const product_controller = require('../controllers/product.controller')
+
+router.get('/test', product_controller.test)
+
+router.post('/create', product_controller.product_create)
+
+router.put('/:id/update', product_controller.product_update)
+
+module.exports = router
